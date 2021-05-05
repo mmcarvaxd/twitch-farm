4d69c27ee57af97990f8c37f71b203c172071be5
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-05-05 14:29:34 -0300

    add express

--- app.js
+++ app.js
@@ -23,3 +23,15 @@ function onConnectedHandler(addr, port) {
         console.log(`Farm rodando a ${minutes} minuto(s)!`)
     }, 60000)
 }
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
